ed8825ae62312c7d5cf4246440678bbef451b188
Isman Firmansyah <isman.firmansyah@gmail.com>
2017-06-08 04:29:40 +0700

    oxauth: download and extract custom oxAuth files

--- oxauth/scripts/entrypoint.sh
+++ oxauth/scripts/entrypoint.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 set -e
 
+download_custom_tar() {
+    if [ ! -z ${GLUU_CUSTOM_OXAUTH_URL} ]; then
+        mkdir -p /tmp/oxauth
+        wget -q ${GLUU_CUSTOM_OXAUTH_URL} -O /tmp/oxauth/custom-oxauth.tar.gz
+        cd /tmp/oxauth
+        tar xf custom-oxauth.tar.gz
+
+        if [ -d /tmp/oxauth/pages ]; then
+            cp -R /tmp/oxauth/pages/* /opt/gluu/jetty/oxauth/custom/pages/
+        fi
+
+        if [ -d /tmp/oxauth/static ]; then
+            cp -R /tmp/oxauth/static/* /opt/gluu/jetty/oxauth/custom/static/
+        fi
+
+        if [ -d /tmp/oxauth/libs ]; then
+            cp -R /tmp/oxauth/libs/* /opt/gluu/jetty/oxauth/lib/ext/
+        fi
+    fi
+}
+
 if [ ! -f /touched ]; then
-    touch /touched
+    download_custom_tar
     python /opt/scripts/entrypoint.py
+    touch /touched
 fi
 
 cd /opt/gluu/jetty/oxauth
