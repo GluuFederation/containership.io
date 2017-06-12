26d7ceeff4bb3eb400d206bac583ce49619429fe
shouro <sourogts@gmail.com>
2017-06-12 19:18:59 +0600

    adding ldif rendering, todo: fix ctx data

--- openldap/scripts/run.py
+++ openldap/scripts/run.py
@@ -1,6 +1,9 @@
 import os
 import re
 import subprocess
+import glob
+import tempfile
+import shutil
 
 import consulate
 
@@ -8,10 +11,13 @@ import consulate
 GLUU_DATASTORE = os.environ.get('GLUU_DATASTORE', 'localhost')
 GLUU_DATASTORE_PORT = os.environ.get('GLUU_DATASTORE_PORT', 8500)
 #TODO env var acesslog_comentout
+TMPDIR = tempfile.mkdtemp()
+
 
 consul = consulate.Consul(host=GLUU_DATASTORE, port=GLUU_DATASTORE_PORT)
 
-#functions taken from setup.py
+
+#START functions taken from setup.py
 def fomatWithDict(text, dictionary):
     text = re.sub(r"%([^\(])", r"%%\1", text)
     text = re.sub(r"%$", r"%%", text)  # There was a % at the end?
@@ -24,23 +30,23 @@ def commentOutText(text):
         lines.append('#%s' % textLine)
     return "\n".join(lines)
 
-# def run(args, cwd=None, env=None, useWait=False):
-#     try:
-#         p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd, env=env)
-#         if useWait:
-#             code = p.wait()
-#             self.logIt('Run: %s with result code: %d' % (' '.join(args), code) )
-#         else:
-#             output, err = p.communicate()
-#             if output:
-#                 self.logIt(output)
-#             if err:
-#                 self.logIt(err, True)
-#     except:
-#         self.logIt("Error running command : %s" % " ".join(args), True)
-#         self.logIt(traceback.format_exc(), True)
+def run(args, cwd=None, env=None, useWait=False):
+    try:
+        p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd, env=env)
+        if useWait:
+            code = p.wait()
+            print 'Run: %s with result code: %d' % (' '.join(args), code)
+        else:
+            output, err = p.communicate()
+            if output:
+                print output
+            if err:
+                print err
+    except:
+        print "Error running command : %s" % " ".join(args)
+        print traceback.format_exc()
+#END functions taken from setup.py
 
-#functions taken from setup.py
 
 def configure_openldap():
     src = '/ldap/templates/slapd.conf'
@@ -71,37 +77,57 @@ def configure_openldap():
         fp.write(fomatWithDict(slapd_template, ctx_data))
 
 
-# def import_ldif_openldap():
-#     ldif_files = [ ldif_base,
-#                    ldif_appliance,
-#                    ldif_attributes,
-#                    ldif_scopes,
-#                    ldif_clients,
-#                    ldif_people,
-#                    ldif_groups,
-#                    ldif_site,
-#                    ldif_scripts,
-#                    ldif_configuration,
-#                    ldif_scim,
-#                    ldif_asimba,
-#                    ldif_passport,
-#                    ldif_passport_config,
-#                    ldif_idp
-#                 ]
+def render_ldif():
+    # TODO: check ctx data and update accordingly with consul KV
+    ctx_data = {
+            "oxauth_client_id": consul.kv.get(''),
+            "oxauth_client_encoded_pw": consul.kv.get(''),
+            "encoded_ldap_pw": consul.kv.get(''),
+            "encoded_ox_ldap_pw": consul.kv.get(''),
+            "inum_appliance": consul.kv.get(''),
+            "hostname": consul.kv.get(''), #self.ldap_failover_hostname() TODO
+            "ox_cluster_hostname": consul.kv.get(''),
+            "ldaps_port": consul.kv.get(''),
+            "ldap_binddn": consul.kv.get(''),
+            "inum_org": consul.kv.get(''),
+            "inum_org_fn": consul.kv.get(''),
+            "org_name": consul.kv.get(''),
+            "scim_rp_client_id": consul.kv.get(''),
+        }
 
-#     cmd = '/opt/symas/bin/slapadd'
-#     config = '/opt/symas/etc/openldap/slapd.conf'
-#     realInstallDir = os.path.realpath(install_dir)
-#     for ldif in ldif_files:
-#         if 'site.ldif' in ldif:
-#             run(['/bin/su', 'ldap', '-c', "cd " + realInstallDir + "; " + " ".join([cmd, '-b', 'o=site', '-f', config, '-l', ldif])])
-#         else:
-#             run(['/bin/su', 'ldap', '-c', "cd " + realInstallDir + "; " + " ".join([cmd, '-b', 'o=gluu', '-f', config, '-l', ldif])])
+    ldif_template_base = '/ldap/templates/ldif'
+    pattern = '/*.ldif'
+    for file_path in glob.glob(ldif_template_base+pattern):
+        with open(file_path, 'r') as fp:
+            template = fp.read()
+        #render
+        rendered_content = template % ctx_data
+        # write to tmpdir
+        with open(os.path.join(TMPDIR, os.path.basename(file_path)), 'w') as fp:
+            fp.write(rendered_content)
+
+
+def import_ldif():
+    slapadd_cmd = '/opt/symas/bin/slapadd'
+    pattern = '/*.ldif'
+    config = '/opt/symas/etc/openldap/slapd.conf'
+    for ldif_file_path in glob.glob(TMPDIR+pattern):
+        if 'site.ldif' in ldif_file_path:
+            run([" ".join([slapadd_cmd, '-b', 'o=site', '-f', config, '-l', ldif_file_path])])
+        else:
+            run([" ".join([slapadd_cmd, '-b', 'o=gluu', '-f', config, '-l', ldif_file_path])])
+
+
+def cleanup():
+    shutil.rmtree(TMPDIR)
 
 
 def run():
     configure_openldap()
-    #import_ldif_openldap()
+    render_ldif()
+    import_ldif()
+    cleanup()
+
 
 if __name__ == '__main__':
     run()
