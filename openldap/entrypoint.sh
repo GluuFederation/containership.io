a61f136aaf7b10f444790e8c698135d4392f1fbd
shouro <sourogts@gmail.com>
2017-06-16 04:58:33 +0600

    fixing log output in foreground mode of slapd

--- openldap/entrypoint.sh
+++ openldap/entrypoint.sh
@@ -9,4 +9,4 @@ fi
 
 #run openldap process
 # TODO: currently using port 1636 but will change to 1389
-exec gosu root /opt/symas/lib64/slapd -d 32768 -u root -g root -h ldap://0.0.0.0:1389/
+exec gosu root /opt/symas/lib64/slapd -d 256 -u root -g root -h ldap://0.0.0.0:1389/
