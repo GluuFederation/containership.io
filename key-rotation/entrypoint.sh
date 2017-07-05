aeeed327e0f6ca4721c52a04ea0e57f4b107bc23
Isman Firmansyah <isman.firmansyah@gmail.com>
2017-07-05 23:22:28 +0700

    key-rotation: use tini to manage entrypoint

--- key-rotation/entrypoint.sh
+++ key-rotation/entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "Preparing env ..."
-
 echo "export GLUU_KV_HOST=${GLUU_KV_HOST}" > /opt/key-rotation/env
 echo "export GLUU_KV_PORT=${GLUU_KV_PORT}" >> /opt/key-rotation/env
 echo "export GLUU_LDAP_URL=${GLUU_LDAP_URL}" >> /opt/key-rotation/env
 echo "export GLUU_KEY_ROTATION_INTERVAL=${GLUU_KEY_ROTATION_INTERVAL}" >> /opt/key-rotation/env
-
-echo "Running cron job ..."
-echo ""
 
 # run cron daemon
 cron
